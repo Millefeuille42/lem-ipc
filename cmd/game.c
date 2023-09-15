f08ff280216dbbb8eea6ee6b6c6350b4ed0d94ed
Millefeuille <mlabouri@student.42.fr>
2023-09-15 14:23:53 +0200

    feat: added idle movement

--- cmd/game.c
+++ cmd/game.c
@@ -15,6 +15,21 @@ t_vec get_first_free(unsigned int map[BOARD_Y][BOARD_X]) {
 	return (t_vec){-1, -1};
 }
 
+void move_to(t_app *app, t_vec to) {
+	if (to.x < 0 || to.y < 0 || to.x >= BOARD_X || to.y >= BOARD_Y) return;
+	int offset = app->cur_pos.x - to.x;
+	offset = offset >= 0 ? offset : offset * -1;
+	if (offset > 1) return;
+	offset = app->cur_pos.y - to.y;
+	offset = offset >= 0 ? offset : offset * -1;
+	if (offset > 1) return;
+
+	if (app->shared->map[to.y][to.x]) return;
+	app->shared->map[app->cur_pos.y][app->cur_pos.x] = 0;
+	app->shared->map[to.y][to.x] = app->team;
+	app->cur_pos = to;
+}
+
 int game_loop(t_app *app) {
 	errno = 0;
 	sem_wait(&app->shared->lock);
@@ -34,6 +49,16 @@ int game_loop(t_app *app) {
 		app->shared->map[first_free.y][first_free.x] = app->team;
 		app->cur_pos = first_free;
 		app->has_spawned = 1;
+	}
+
+	// Idle movement
+	char next_move = (char)(random() % 4);
+	switch (next_move) {
+		case 0: move_to(app, (t_vec){app->cur_pos.x - 1, app->cur_pos.y}); break;
+		case 1: move_to(app, (t_vec){app->cur_pos.x + 1, app->cur_pos.y}); break;
+		case 2: move_to(app, (t_vec){app->cur_pos.x, app->cur_pos.y - 1}); break;
+		case 3: move_to(app, (t_vec){app->cur_pos.x, app->cur_pos.y + 1}); break;
+		default: break;
 	}
 	// TODO do game stuff
 	//  if player loses, return `non 0`
